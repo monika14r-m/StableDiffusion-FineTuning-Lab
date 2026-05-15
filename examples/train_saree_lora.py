2c7ee5d7d990e6d716d1d50721d7c71084a1aa65
Monika <monikar_icb2023@ksit.edu.in>
2026-05-16 01:33:09 +0530

    Update train_saree_lora.py

--- examples/train_saree_lora.py
+++ examples/train_saree_lora.py
@@ -34,3 +34,33 @@ os.makedirs(output_dir, exist_ok=True)
 
 print(f"Ready to train on {train_data_dir} and save to {output_dir}")
 # TODO: Add dataset loader + training loop
+
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+import glob
+
+# Simple dataset for saree images
+class SareeDataset(Dataset):
+    def __init__(self, img_dir, transform=None):
+        self.files = glob.glob(f"{img_dir}/*.png") + glob.glob(f"{img_dir}/*.jpg")
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, idx):
+        img = Image.open(self.files[idx]).convert("RGB")
+        if self.transform:
+            img = self.transform(img)
+        return {"pixel_values": img}
+
+# Loader
+dataset = SareeDataset(train_data_dir)
+loader = DataLoader(dataset, batch_size=2, shuffle=True)
+
+# Training loop placeholder
+for epoch in range(5):  # small test run
+    for batch in loader:
+        # TODO: forward pass with pipe.unet + LoRA
+        # loss.backward(), optimizer.step()
+        print("Training step on batch")
